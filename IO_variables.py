265798aab8736eada1604195a03e8324b29abf50
eirene-rene <rebekahsofo@gmail.com>
2022-04-02 21:14:47 +0000

    finished input,output and variables

---
+++ IO_variables.py
@@ -0,0 +1,4 @@
+name = input("what is your name: ") # asking for name
+age = input("what is your age: ") # asking for age
+
+print(f"your name is {name} and you are {age} years old")
--- printed_name.py
+++
@@ -1,4 +0,0 @@
-name = input("what is your name: ")
-age = input("what is your age: ")
-
-print(f"your name is {name} and you are {age} years old")
