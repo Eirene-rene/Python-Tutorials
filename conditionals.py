c7956ff8111f0fa0f41004dd52de0fc3acbae0f3
eirene-rene <rebekahsofo@gmail.com>
2022-04-02 21:49:46 +0000

    finished conditionals

---
+++ conditionals.py
@@ -0,0 +1,21 @@
+# yaw = 4
+# if yaw == 4:
+#     print("My name is Rebeccas")
+# else:
+#     print("What is your name: ")
+
+# THE NORMAL DATA TYPE FOR INPUT IS A STRING, SO WHEN DEALING WITH NUMBERS YOU HAVE TO CONVERT IT.
+# age = int(input("what is your age: "))
+# print(age+1)
+
+age = int(input("what is your age: "))
+if age > 20:
+    print("you are 21 or above")
+elif age < 20:
+    print("you are 19 or below ")
+elif age == 20:
+    print("you are 20 years old")
+else:
+    print("invalid input")
+
+
